f70d6afbd6a7e21db45b0ba46b9e92b9802ac549
Gerard San Juan <49268224+Tiwala@users.noreply.github.com>
2022-09-28 17:46:06 -0500

    made basic header and footer in layout

--- components/layout/Footer.js
+++ components/layout/Footer.js
@@ -1,8 +1,28 @@
 import React from 'react'
+import footerStyle from '../../styles/Footer.module.css'
+
 
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <>
+        <div className={footerStyle.footerSpacer}></div>
+        <footer className={footerStyle.footer}>
+            <div className={footerStyle.textWrapper}>
+                <a>
+                    Technologies
+                </a>
+                <a>
+                    copyright blah
+                </a>
+                <a>
+                    about
+                </a>
+                <a className={footerStyle.projectName}>
+                    cool name of project
+                </a>
+            </div>
+        </footer>
+    </>
   )
 }
 
