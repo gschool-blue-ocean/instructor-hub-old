c66cf9fcb2d168f10f2a6214a4fcd7761b6d6a38
BlazingAces <1002charles@gmail.com>
2022-09-29 15:45:38 -0400

    Created the Cohort Container, added a CSS stylesheets for the CohortContainer, CohortOverall, ChoortSpacer, Added divs to the CohortOveral, CohortSpcer, and studentSummary.

--- components/main_page/CohortInformation.js/CohortOveral.js
+++ components/main_page/CohortInformation.js/CohortOveral.js
@@ -1,8 +1,8 @@
-import React from 'react'
+import overallStlyes from '../../../styles/CohortOveral.module.css'
 
 const CohortOveral = () => {
   return (
-    <div>CohortOveral</div>
+    <div className = {overallStlyes.overalBorder}>CohortOveral</div>
   )
 }
 
--- components/main_page/CohortInformation.js/StudentSummary.js
+++ components/main_page/CohortInformation.js/StudentSummary.js
@@ -4,7 +4,9 @@ import studentStlye from '../../../styles/StudentSummary.module.css'
 const StudentSummary = () => {
 
   return (
-    <div className = {studentStlye.border}>Student Summary</div>
+    <div className = {studentStlye.border}>
+      <div className = {studentStlye.topBorder}>Student Summary</div>
+    </div>
   )
 }
 
