023352a541810ea0eb78407627b2f33014e16395
mbcohen777@gmail.com <mbcohen777@gmail.com>
2022-09-29 15:39:31 -0400

    m

--- ocean-seed.sql
+++ ocean-seed.sql
@@ -2,8 +2,9 @@ DROP TABLE IF EXISTS students CASCADE;
 DROP TABLE IF EXISTS cohorts CASCADE;
 DROP TABLE IF EXISTS pairs CASCADE;
 DROP TABLE IF EXISTS notes CASCADE;
-DROP TABLE IF EXISTS scores CASCADE;
-
+DROP TABLE IF EXISTS projects CASCADE;
+DROP TABLE IF EXISTS learn CASCADE;
+DROP TABLE IF EXISTS score CASCADE;
 
 CREATE TABLE students (
 name_first TEXT,
@@ -45,13 +46,26 @@ note_date DATE
 );
 
 
-CREATE TABLE scores (
+CREATE TABLE projects (
 student_id INT,
-cohort_id INT,
-learn_complete TEXT,
-learn_scores TEXT [],
-projects_complete TEXT,
-project_scores INT []
+name_first TEXT,
+name_last TEXT,
+cohort TEXT,
+project1 INT,
+project2 INT,
+project3 INT,
+projects_avg
+);
+
+CREATE TABLE learn (
+student_id INT,
+cohort TEXT,
+name_first TEXT,
+name_last TEXT,
+learn1 INT,
+learn2 INT,
+learn3 INT,
+learn_avg INT
 );
 
 
@@ -63,8 +77,10 @@ INSERT INTO pairs (cohort, student1_ln, student2_ln)
   VALUES ('MCSP13', 'Testor', 'Mon');
 INSERT INTO notes (name_first, name_last, instructor_notes, SEIR_notes, note_date) 
   VALUES ('John', 'Testor', 'Meh', 'Meh','02/02/2022');
-INSERT INTO scores (learn_complete, learn_scores) VALUES ('learn1','{"99"}'),('learn2','{"88"}'),('learn3','{"77"}'),('learn4','{"66"}');
-INSERT INTO scores (projects_complete, project_scores) VALUES ('project1','{"99"}'),('project2','{"88"}'),('project3','{"77"}'),('project4','{"66"}');
+INSERT INTO projects (name_first, name_last, cohort, project1, project2, project3) 
+  VALUES ('John', 'Testor', 'MCSP13', '99', '88','77');
+INSERT INTO learn (name_first, name_last, cohort, learn1, learn2, learn3) 
+  VALUES ('John', 'Testor', 'MCSP13', '99', '88','77');
 
 
 ALTER TABLE students ADD COLUMN student_id SERIAL PRIMARY KEY;
@@ -72,12 +88,12 @@ ALTER TABLE cohorts ADD COLUMN cohort_id SERIAL PRIMARY KEY;
 ALTER TABLE pairs ADD COLUMN pair_id SERIAL PRIMARY KEY;
 
 
-
+ALTER TABLE students ADD CONSTRAINT student_learn FOREIGN KEY(learn_avg) REFERENCES learn(learn_avg);
+ALTER TABLE students ADD CONSTRAINT student_project FOREIGN KEY(project_avg) REFERENCES projects(project_avg);
 ALTER TABLE pairs ADD CONSTRAINT pairs_cohort FOREIGN KEY(cohort_id) REFERENCES cohorts(cohort_id);
 ALTER TABLE notes ADD CONSTRAINT notes_student FOREIGN KEY(student_id) REFERENCES students(student_id);
-ALTER TABLE scores ADD CONSTRAINT scores_student FOREIGN KEY(student_id) REFERENCES students(student_id);
-ALTER TABLE scores ADD CONSTRAINT scores_cohort FOREIGN KEY(cohort_id) REFERENCES cohorts(cohort_id);
-
+ALTER TABLE projects ADD CONSTRAINT project_student FOREIGN KEY(student_id) REFERENCES students(student_id);
+ALTER TABLE learn ADD CONSTRAINT learn_student FOREIGN KEY(student_id) REFERENCES students(student_id);
 
 
 INSERT INTO students (name_first, name_last, server_side_test, client_side_test, soft_skills, cohort, ETS_date) 
@@ -88,5 +104,7 @@ INSERT INTO pairs (cohort, student1_ln, student2_ln)
   VALUES ('MCSP13', 'Testor', 'Mon');
 INSERT INTO notes (name_first, name_last, instructor_notes, SEIR_notes, note_date) 
   VALUES ('Jo', 'Mon', 'Meh', 'Meh','02/02/2022');
-INSERT INTO scores (learn_complete, learn_scores) VALUES ('learn1','{"99"}'),('learn2','{"88"}'),('learn3','{"77"}'),('learn4','{"66"}');
-INSERT INTO scores (projects_complete, project_scores) VALUES ('project1','{"99"}'),('project2','{"88"}'),('project3','{"77"}'),('project4','{"66"}');
+INSERT INTO projects (name_first, name_last, cohort, project1, project2, project3) 
+  VALUES ('Jo', 'Mon', 'MCSP13', '99', '88','77');
+INSERT INTO learn (name_first, name_last, cohort, learn1, learn2, learn3) 
+  VALUES ('Jo', 'Mon', 'MCSP13', '99', '88','77');
