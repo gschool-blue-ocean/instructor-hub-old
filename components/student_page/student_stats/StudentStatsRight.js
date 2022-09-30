1a790c8a3bc7b238c9a08a08d7d641a9e97ee77e
Gerard San Juan <49268224+Tiwala@users.noreply.github.com>
2022-09-30 16:01:39 -0500

    progressed on student page, added image, basic styling for left side

--- components/student_page/student_stats/StudentStatsRight.js
+++ components/student_page/student_stats/StudentStatsRight.js
@@ -1,8 +1,8 @@
-
+import style from '../../../styles/StudentStatsRight.module.css'
 
 const StudentStatsRight = () => {
   return (
-    <div>StudentStatsRight</div>
+    <div className={style.rightMainDiv}>StudentStatsRight</div>
   )
 }
 
