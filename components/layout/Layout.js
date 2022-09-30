021cdb77588fa225db82ee9f25881bfd477d6b4a
daryle <daryle.tan@gmail.com>
2022-09-30 17:46:47 -0500

    added appropriate object paths to each get requests

--- components/layout/Layout.js
+++ components/layout/Layout.js
@@ -27,27 +27,53 @@ const Layout = ({ children }) => {
   const [projectGrades, setProjectGrades] = useRecoilState(projectGradesState);
 
   useEffect(() => {
-    axios.get("/api/cohorts").then((res) => setCohorts(res.data[0]));
+    axios.get("/api/cohorts").then((res) => {
+      setCohorts(res.data.cohorts[0]);
+      console.log(res.data.cohorts[0]);
+    });
 
-    axios.get("/api/learn").then((res) => setLearn(res.data[0]));
+    axios.get("/api/learn").then((res) => {
+      setLearn(res.data.learn[0]);
+      console.log(res.data.learn[0]);
+    });
 
-    axios.get("/api/notes").then((res) => setNotes(res.data[0]));
+    axios.get("/api/notes").then((res) => {
+      console.log(res);
+      setNotes(res.data.notes[0]);
+      console.log(res.data.notes[0]);
+    });
 
-    axios.get("/api/codingGroup").then((res) => setCodingGroup(res.data[0]));
+    axios.get("/api/codingGroups").then((res) => {
+      console.log(res);
+      setCodingGroup(res.data.codingGroups[0]);
+      console.log(res.data.codingGroups[0]);
+    });
 
-    axios.get("/api/projects").then((res) => setProjects(res.data[0]));
+    axios.get("/api/projects").then((res) => {
+      setProjects(res.data.projects[0]);
+      console.log(res.data.projects[0]);
+    });
 
-    axios.get("/api/students").then((res) => setStudents(res.data[0]));
+    axios.get("/api/students").then((res) => {
+      setStudents(res.data.students[0]);
+      console.log(res.data.students[0]);
+    });
 
-    axios.get("/api/learnGrades").then((res) => setLearnGrades(res.data[0]));
+    axios.get("/api/learnGrades").then((res) => {
+      setLearnGrades(res.data.learnGrades[0]);
+      console.log(res.data.learnGrades[0]);
+    });
 
-    axios
-      .get("/api/projectGrades")
-      .then((res) => setProjectGrades(res.data[0]));
+    axios.get("/api/projectGrades").then((res) => {
+      setProjectGrades(res.data.projectGrades[0]);
+      console.log(res.data.projectGrades[0]);
+    });
 
-    axios
-      .get("/api/assignedGroup")
-      .then((res) => setAssignedGroup(res.data[0]));
+    axios.get("/api/assignedGroup").then((res) => {
+      console.log(res);
+      setAssignedGroup(res.data.studentGroupings[0]);
+      console.log(res.data.studentGroupings[0]);
+    });
   }, []);
 
   return (
