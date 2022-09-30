7f3800157c19656d878d2d83ce8cd54880b3d767
BlazingAces <1002charles@gmail.com>
2022-09-30 10:46:36 -0400

    Had to delete  the middle of the table. My OCD hated the way it looked so I restarted it. I believe I found a good reference point to use for creating a better looking one

--- components/main_page/CohortInformation.js/StudentSummary.js
+++ components/main_page/CohortInformation.js/StudentSummary.js
@@ -1,43 +1,26 @@
 import React from 'react'
-import studentStlye from '../../../styles/StudentSummary.module.css'
+import studentStyle from '../../../styles/StudentSummary.module.css'
 
 const StudentSummary = () => {
 
   return (
-    <div className = {studentStlye.border}>
+    <div className = {studentStyle.border}>
       <div>
-        <div className = {studentStlye.topBorder}>
-          <div className = {studentStlye.selectRow}>
-            <div className = {studentStlye.selectAllBox}>
-              <input className= {studentStlye.checkBox} type= "checkbox" name= "selectAll"/>
+        <div className = {studentStyle.topBorder}>
+          <div className = {studentStyle.selectRow}>
+            <div className = {studentStyle.selectAllBox}>
+              <input className= {studentStyle.checkBox} type= "checkbox" name= "selectAll"/>
                 <label htmlFor = "selectMe"> Select/Deselect All</label>
             </div>
-            <div className = {studentStlye.addGit}>
-              <a className = {studentStlye.gitBtn}>
+            <div className = {studentStyle.addGit}>
+              <a className = {studentStyle.gitBtn}>
                 <span>Github Accounts</span>
               </a>
             </div>
           </div>
         </div>
-        <div className= {studentStlye.middleBorder}>
-          <div className= {studentStlye.topRow}>
-            <div className= {studentStlye.column}>
-              <div className= {studentStlye.columnHeaders}>
-                <span>Name</span>
-              </div>
-              <div className= {studentStlye.columnHeaders}>
-                <span>Technical Score</span>
-              </div>
-              <div className= {studentStlye.columnHeaders}>
-                <span>Progress</span>
-              </div>
-              <div className= {studentStlye.columnHeaders}>
-                <span>Comments</span>
-              </div>
-              <div className= {studentStlye.columnHeaders}>
-                <span></span>
-              </div>
-            </div>
+        <div className= {studentStyle.middleBorder}>
+          <div className= {studentStyle.topRow}>
           </div>
         </div>
       </div>
