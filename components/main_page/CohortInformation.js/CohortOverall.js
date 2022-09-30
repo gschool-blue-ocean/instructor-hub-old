6d88768aa0b8c67a61e44bc9198376abb66e9888
Zach Hudson <99704012+Self-Zachtualization@users.noreply.github.com>
2022-09-30 13:19:58 -0600

    Update CohortOverall.js

--- components/main_page/CohortInformation.js/CohortOverall.js
+++ components/main_page/CohortInformation.js/CohortOverall.js
@@ -1,8 +1,8 @@
-import overallStlyes from '../../../styles/CohortOverall.module.css'
+import overallStyles from '../../../styles/CohortOverall.module.css'
 
 const CohortOverall = () => {
   return (
-    <div className = {overallStlyes.overallBorder}>CohortOveral</div>
+    <div className = {overallStyles.overallBorder}>CohortOverall</div>
   )
 }
 
