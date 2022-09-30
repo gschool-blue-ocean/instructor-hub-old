c027ed4e474e06f00711df3cc87abbfccc57c280
Gerard San Juan <49268224+Tiwala@users.noreply.github.com>
2022-09-30 16:59:02 -0500

    completed wireframe for the student page

--- components/student_page/student_stats/StudentStatsRight.js
+++ components/student_page/student_stats/StudentStatsRight.js
@@ -2,7 +2,18 @@ import style from '../../../styles/StudentStatsRight.module.css'
 
 const StudentStatsRight = () => {
   return (
-    <div className={style.rightMainDiv}>StudentStatsRight</div>
+    <div className={style.rightMainDiv}>
+      <div className={style.progress}>
+        <div className={style.pot}>Progress over time</div>
+        <img src="https://freepngimg.com/thumb/stock_market/25650-5-stock-market-graph-up-transparent-background.png" className={style.graph} />
+      </div>
+      <div className={style.learnAverage}>
+        learn Average
+      </div>
+      <div className={style.projAverage}>
+        project Average
+      </div>
+    </div>
   )
 }
 
