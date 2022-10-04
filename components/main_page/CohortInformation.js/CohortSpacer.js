5c3bcd21b0f0bba2297f526291fb51d957011d61
BlazingAces <1002charles@gmail.com>
2022-10-04 15:20:04 -0400

    Added the three dot icons, and the comment icon. Additionally, added css to prevent the information from going outside the border.

--- components/main_page/CohortInformation.js/CohortSpacer.js
+++ components/main_page/CohortInformation.js/CohortSpacer.js
@@ -2,23 +2,19 @@ import spacerStyle from '../../../styles/CohortSpacer.module.css'
 
 const CohortSpacer = () => {
   return (
-<<<<<<< HEAD
     <div className = {spacerStyle.spacerContainer}>
       <div>
         <div className = {spacerStyle.centerContainer}>
           <div className = {spacerStyle.center}>
             <div className = {spacerStyle.newCohort_btn}>
               <a href="#">
-                <span className = {spacerStyle.newCohort_btn}>New Cohort</span>
+                <span>New Cohort</span>
               </a>
             </div>
           </div>
         </div>
       </div>
     </div>
-=======
-    <div className = {spacerStyle.spacerContainer}></div>
->>>>>>> 015b8a71f32b99237c19b6d02dc6fc3fe49b75c2
   )
 }
 
