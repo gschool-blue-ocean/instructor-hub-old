ad01a57f2ba5ae1bd885ee300a6aa9c8421734db
BlazingAces <1002charles@gmail.com>
2022-10-04 18:14:28 -0400

    Created the modal for the github account button. Styling and backend accessablity needed. Moving on to make the comment box for the comment icon. Something to Note, I did not create a seperate page for the modals,I made them from scratch on the Student Summary page.

--- components/main_page/CohortInformation.js/StudentSummary.js
+++ components/main_page/CohortInformation.js/StudentSummary.js
@@ -3,6 +3,7 @@ import gitStyle from '../../../styles/GitHub.module.css'
 import React, { useState, useEffect } from "react"
 
 
+
 const StudentSummary = () => {
   const [showGitHub, setShowGitHub] = useState(false);
 
@@ -18,7 +19,7 @@ const StudentSummary = () => {
             </div>
             <div className = {studentStyle.addGit}>
               <a className = {studentStyle.gitBtn}>
-                <span onClick= {() => setShowGitHub(true)} className ={` ${studentStyle.gitBtn_medium} ${studentStyle.span}`}>Github Accounts</span>
+                <span onClick={() => setShowGitHub(true)} className = {` ${studentStyle.gitBtn_medium} ${studentStyle.span}`}>Github Accounts</span>
               </a>
             </div>
           </div>
@@ -103,26 +104,50 @@ const StudentSummary = () => {
           </div>
         </div>
       </div>
-      {/* {showGitHub ? (
+  {showGitHub ? (
         <div>
           <div className= {gitStyle.gitContainer}>
             <div>
-             <div className= {gitStyle.listHeader}>
-              <div>
-                <div className= {gitStyle.headerInfo}>
-                  <div className= {gitStyle.info}>
-                    <svg className= {gitStyle.personIcon}>
-                      <path fillRule="evenodd" d="M10.5 5a2.5 2.5 0 11-5 0 2.5 2.5 0 015 0zm.061 3.073a4 4 0 10-5.123 0 6.004 6.004 0 00-3.431 5.142.75.75 0 001.498.07 4.5 4.5 0 018.99 0 .75.75 0 101.498-.07 6.005 6.005 0 00-3.432-5.142z"></path>
-                    </svg> 
-                    " 0 members "
+              <div className= {gitStyle.listHeader}>
+                <div>
+                  <div className= {gitStyle.headerInfo}>
+                    <div className= {gitStyle.info}><svg className= {gitStyle.personIcon}><path fillRule="evenodd" d="M10.5 5a2.5 2.5 0 11-5 0 2.5 2.5 0 015 0zm.061 3.073a4 4 0 10-5.123 0 6.004 6.004 0 00-3.431 5.142.75.75 0 001.498.07 4.5 4.5 0 018.99 0 .75.75 0 101.498-.07 6.005 6.005 0 00-3.432-5.142z"></path></svg> 0 members </div>
                   </div>
+                  <div onClick={() => setShowGitHub(false)} className= {gitStyle.close}></div>
                 </div>
               </div>
-             </div>
+              <ul className= {gitStyle.tableList}>
+                <li className= {gitStyle.tableListItem}>
+                  <div className= {gitStyle.tableListCell}>
+                    <span className= {gitStyle.frameLeft}>
+                      <a className= {gitStyle.frameInline} href= "#">
+                        <div className= {gitStyle.avatar}></div>
+                      </a>
+                    </span>
+                  </div>
+                  <div className= {gitStyle.tableListName}>
+                    <a className= {gitStyle.frameInclineName}> Student 1 </a>
+                    <span className= {gitStyle.codeName}>TantalizingTickler</span>
+                  </div>
+                </li>
+                <li className= {gitStyle.tableListItem}>
+                  <div className= {gitStyle.tableListCell}>
+                    <span className= {gitStyle.frameLeft}>
+                      <a className= {gitStyle.frameInline} href= "#">
+                        <div className= {gitStyle.avatar}></div>
+                      </a>
+                    </span>
+                  </div>
+                  <div className= {gitStyle.tableListName}>
+                    <a className= {gitStyle.frameInclineName}> Student 2 </a>
+                    <span className= {gitStyle.codeName}> BurtMFReynolds</span>
+                  </div>
+                </li>
+              </ul> 
             </div>
           </div>
         </div>
-      ) : null} */}
+      ) : null}
     </div>
   )
 }
