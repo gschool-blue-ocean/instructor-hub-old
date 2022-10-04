958abf454b20c164c54fbe00e06c673eb5ef4758
mmartin440 <105955175+mmartin440@users.noreply.github.com>
2022-10-03 22:13:26 -0700

    Edited on studentSummary, cohortContainer components. I edited some words out that don't need to be there and added styling to each of them. I also created about page with styling. Added the background color in global css so the color can be seen every component.

--- pages/about.js
+++ pages/about.js
@@ -1,8 +1,10 @@
 import style from '../styles/About.module.css'
 import Image from 'next/image'
-
+import NavBar from '../components/main_page/NavBar'
 const about = () => {
   return (
+     <>
+    <NavBar/>
     <div className={style.container}>
       <div className={style.header}>
         <h2>
@@ -26,7 +28,7 @@ const about = () => {
           </div>
           <div className={style.team}>
             <Image src='/pic1.jpg' height={200} width={200}/>
-            <div className={style.name}>Gerard San Juan</div>
+            <div className={style.name}>Zachary Hudson</div>
             <div className={style.about} >
               This is about me blah blah blah and more blah here, creative blahs only allowed or no, 
               this is alot more difficult then I thought 
@@ -39,7 +41,7 @@ const about = () => {
           <div className={style.team}>
             <Image src='/pic1.jpg' height={200} width={200}/>
 
-            <div className={style.name}>Zach</div>
+            <div className={style.name}>Gerard San Juan</div>
             <div className={style.about} >
               This is about me blah blah blah and more blah here, creative blahs only allowed or no, 
               this is alot more difficult then I thought 
@@ -103,6 +105,18 @@ const about = () => {
           <div className={style.team}>
             <Image src='/pic1.jpg' height={200} width={200}/>
 
+            <div className={style.name}>Samuel Chavez</div>
+            <div className={style.about} >
+              This is about me blah blah blah and more blah here, creative blahs only allowed or no, 
+              this is alot more difficult then I thought 
+            </div>
+            <div className={style.socialLink}>
+              <a className={style.links} href='#'><i>GH Icon</i></a>
+              <a className={style.links} href='#'><i>LI Icon</i></a>
+            </div>
+          </div>
+          <div className={style.team}>
+            <Image src='/pic1.jpg' height={200} width={200}/>
             <div className={style.name}>Martha Martin</div>
             <div className={style.about} >
               This is about me blah blah blah and more blah here, creative blahs only allowed or no, 
@@ -115,19 +129,7 @@ const about = () => {
           </div>
           <div className={style.team}>
             <Image src='/pic1.jpg' height={200} width={200}/>
-            <div className={style.name}>Randy</div>
-            <div className={style.about} >
-              This is about me blah blah blah and more blah here, creative blahs only allowed or no, 
-              this is alot more difficult then I thought 
-            </div>
-            <div className={style.socialLink}>
-              <a className={style.links} href='#'><i>GH Icon</i></a>
-              <a className={style.links} href='#'><i>LI Icon</i></a>
-            </div>
-          </div>
-          <div className={style.team}>
-            <Image src='/pic1.jpg' height={200} width={200}/>
-            <div className={style.name}></div>
+            <div className={style.name}>Randy Fernandez</div>
             <div className={style.about} >
               This is about me blah blah blah and more blah here, creative blahs only allowed or no, 
               this is alot more difficult then I thought 
@@ -139,6 +141,7 @@ const about = () => {
           </div>
       </div>
     </div>
+  </>
   )
 }
 
