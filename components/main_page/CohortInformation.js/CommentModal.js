2278d844bbedaa8a8666a3d11dd7c0c606339508
BlazingAces <1002charles@gmail.com>
2022-10-04 21:20:59 -0400

    Modal have been seperated into there own js file. Still working on the comment box styling at this time

---
+++ components/main_page/CohortInformation.js/CommentModal.js
@@ -0,0 +1,34 @@
+import { useState } from "react";
+import commentStyle from '../../../styles/CommentModal.module.css'
+
+const CommentModal = ({ showCommentModal, setShowCommenttModal, onClose }) => {
+  
+    return (
+      <>
+  {showCommentModal ? (
+        <div>
+        <div className={commentStyle.background} onClick={onClose}></div>
+            <div className= {commentStyle.container}>
+            <div className = {commentStyle.top_bar}></div>
+                <div>
+                <div className= {commentStyle.commentHeader}>
+                    <div>
+                    <div onClick={onClose} className= {commentStyle.close}></div>
+                    <div className= {commentStyle.studentName}> Student 1 </div>
+                    </div>
+                </div>
+                    <div className= {commentStyle.content_container}>
+                    <div className= {commentStyle.content}>
+                        <div className= {commentStyle.introSentence}> Please enter comments that you feel are relevant below:</div>
+                        <textarea className= {commentStyle.app_notes} placeholder= "Type your comment here"></textarea>
+                    </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        ) : null}
+      </>
+    );
+  };
+  
+  export default CommentModal;
