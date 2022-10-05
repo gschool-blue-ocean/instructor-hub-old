78d0d62196711d4fb3ed42141be0ab099c89f078
mmartin440 <105955175+mmartin440@users.noreply.github.com>
2022-10-05 10:26:08 -0700

    Deleted Comp StudentNavBar, StudentProfile, StudentStatsLeft, StudentStatsRight, I added three componenets for status left and right, And Student Page CSS

--- components/student_page/StudentMainBody.js
+++ components/student_page/StudentMainBody.js
@@ -1,14 +1,25 @@
+import styles from '../../styles/StudentPage.module.css'
+import StudentStatus from '../student_page/student_stats/StudentStatus.js'
 import NavBar from '../main_page/NavBar.js'
-import StudentProfile from './student_stats/StudentProfile'
-import style from '../../styles/StudentMainBody.module.css'
 
 const StudentMainBody = () => {
   return (
-    <div className={style.studentMainBody}>
-      <NavBar/>
-      <div style={{height: '50px'}}></div>
-      <StudentProfile />
+    <>
+    <NavBar />
+    <div className={styles.container}>
+      <div className={styles.containerTitle}>
+        <img className={styles.picture} alt='student pic' src='/pic1.jpg' width={80} height={80}/>
+        <div className={styles.title}>
+          <div className={styles.studentName}>First LastName</div>
+          <div className={styles.gitTransCon}>
+            <p className={styles.etsDate}>ETS DATE: March 20, 2023</p>
+            <p className={styles.gitHub}>GitHub Username: student123A</p>
+          </div>
+        </div>
+      </div>
+      <StudentStatus/>
     </div>
+  </>
   )
 }
 
