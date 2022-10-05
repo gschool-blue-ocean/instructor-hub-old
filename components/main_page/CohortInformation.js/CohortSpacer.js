5b91ec306ec947933acedde1e71be4be1577368d
Gerard San Juan <49268224+Tiwala@users.noreply.github.com>
2022-10-04 23:53:00 -0500

    added New Cohort button in Cohort Spacer, button spawns NewCohortModal, no contents thanks Zach

--- components/main_page/CohortInformation.js/CohortSpacer.js
+++ components/main_page/CohortInformation.js/CohortSpacer.js
@@ -1,8 +1,28 @@
 import spacerStyle from '../../../styles/CohortSpacer.module.css'
+import { useState } from 'react'
+import NewCohortModal from './NewCohortModal'
 
 const CohortSpacer = () => {
+  const [newCohortModal, showNewCohortModal] = useState(false)
+
+  const newCohort = () => {
+    showNewCohortModal(!newCohortModal)
+  }
+
   return (
-    <div className = {spacerStyle.spacerContainer}></div>
+    <>
+      <div className = {spacerStyle.spacerContainer}>
+        <a className = {spacerStyle.gitBtn}>
+          <span onClick={newCohort} className ={` ${spacerStyle.gitBtn_medium} ${spacerStyle.span}`}>Add Cohort</span>
+        </a>
+      </div>
+      <NewCohortModal
+      showNewCohortModal={showNewCohortModal}
+      newCohortModal={newCohortModal}
+      onClose={() => {
+        showNewCohortModal(false);
+      }}/>
+    </>
   )
 }
 
