98935ebd6ee86d80f5f7ccb9ba34fbbcfce4d715
A-Cobble <alex.cobble42@gmail.com>
2022-10-06 21:23:48 -0700

    added a box shadow to the login and signup pages. started on the logic for creating an account.

--- components/login_page/SignUp.js
+++ components/login_page/SignUp.js
@@ -1,8 +1,35 @@
-import React from 'react'
+import { useState } from 'react'
 import styles from '../../styles/signUp.module.css'
 import Link from 'next/link'
+import { useRecoilState } from 'recoil'
+import { usersState } from '../state'
+import axios from 'axios'
 
 const SignUp = () => {
+    const [username, setUsername] = useRecoilState(usersState);
+    const [password, setPassword] = useRecoilState(usersState);
+    const [default_cohort, setDefault_cohort] = useRecoilState(usersState);
+    const [displayCohortModal, setDisplayCohortModal] = useState(false)
+    const [displayAsanaKeyModal, setDisplayAsanaKeyModal] = useState(false)
+
+    const createUsername= (e)=> {
+        console.log(e.target.value)
+        setUsername(e.target.value)
+    }
+
+    const createPassword = (e) => {
+        console.log(e.target.value)
+        setPassword(e.target.value)
+    }
+    function showDisplayCohortModal(e){
+        setDisplayCohortModal(true)
+    }
+
+    function createUserProfile() {
+        axios.post('/api/users', {})
+        .then(res.data[0])
+    }
+
   return (
     <div className={styles.signUpContainer}>
         <div className={styles.signUpLargeTextContainer}>
@@ -20,36 +47,36 @@ const SignUp = () => {
                                     <div className={styles.signUp}>
                                         Username
                                     </div>
-                                    <input className={styles.inputFields} type='text'></input>
+                                    <input className={styles.inputFields} onChange={(e) => createUsername(e)} type='text' required></input>
                                 </div>
                                 <div className={styles.accountInputContainer}>
                                     <div className={styles.signUp}>
                                         Password
                                     </div>
-                                    <input className={styles.inputFields} type='password'></input>
+                                    <input className={styles.inputFields} onChange={(e) => createPassword(e)} type='password' required></input>
                                 </div>
                                 <div className={styles.accountInputContainer}>
                                     <div className={styles.signUp}>
                                         Confirm Password
                                     </div>
-                                    <input className={styles.inputFields} type='password'></input>
+                                    <input className={styles.inputFields} type='password' required></input>
                                 </div>
                                 
-                                <div className={styles.cohortSelectorDiv}>{'Select Cohort: '}
-                                    <select className={styles.cohortSelector}>
+                                {/* <div className={styles.cohortSelectorDiv}>{'Select Cohort: '}
+                                    <select className={styles.cohortSelector}> */}
                                         {/* add a for each so that it will auto create the options for us*/}
-                                        <option value="volvo">MCSP-13 Bravo</option>
-                                        <option value="saab">MCSP-13 Alpha</option>
-                                        <option value="fiat">MCSP-12</option>
-                                        <option value="audi">MCSP-11</option>
-                                    </select>
-                                </div>
+                                        {/* <option value="">MCSP-13 Bravo</option>
+                                        <option value="">MCSP-13 Alpha</option>
+                                        <option value="">MCSP-12</option>
+                                        <option value="">MCSP-11</option>
+                                    </select> */}
+                                {/* </div> */}
                                 <div className={styles.accessTokenDiv}>
                                     {'Asana API KEY: '}
-                                    <input type='text' className={styles.inputTokenField} placeholder='Optional'></input>
+                                    <input type='text' className={styles.inputTokenField} placeholder='Required' required></input>
                                 </div>
                                 <div className={styles.signInBtnContainer}>
-                                    <button type='submit' className={styles.signInBtn}>Create Account</button>
+                                    <button type='submit' className={styles.signInBtn} onClick={(e) => showDisplayCohortModal(e)}>Create Account</button>
                                 </div>
                                 <div>
                                     {"Already have an account? Click "}
