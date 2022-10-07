1398cf7b97349c57e3f9d5b3fb5b27da407c489f
daryle <daryle.tan@gmail.com>
2022-10-06 23:25:07 -0500

    added post request to projects.js and projectGrades.js and to learnGrades.js

--- pages/api/projectGrades.js
+++ pages/api/projectGrades.js
@@ -21,6 +21,18 @@ export default async function projectGradesHandler(req, res) {
       console.error(err);
       return res.status(500).json({ msg: "Messed up on our end" });
     }
+  } else if (req.method === "POST") {
+    try {
+      const { student_id, project_id, project_grade } = req.body;
+      console.log(req.body);
+      const createProjectGrades = await sql`
+               INSERT INTO project_grades ( student_id, project_id, project_grade )
+               VALUES ( ${student_id}, ${project_id}, ${project_grade}) RETURNING *`;
+      res.status(200).json({ createProjectGrades });
+    } catch (error) {
+      console.error("Bad news in index api: ", error);
+      return res.status(500).json({ msg: "Messed up on our end" });
+    }
   } else {
     res.status(400).json({ msg: "You messed up" });
   }
