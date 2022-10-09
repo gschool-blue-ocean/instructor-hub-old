84cdcbd0b24f89c189dab6efe39cbb74712c50a2
daryle <daryle.tan@gmail.com>
2022-10-09 00:08:58 -0500

    created patch request for projects and project_grades and indiviual get request for projects

--- pages/api/projectsAndProjectGradesId/[projectsAndProjectGradesid].js
+++ pages/api/projectsAndProjectGradesId/[projectsAndProjectGradesid].js
@@ -25,28 +25,17 @@ export default async function getLearnAndLearnGradesId(req, res) {
       console.error(err);
       return res.status(500).json({ msg: "Messed up on our end" });
     }
-    // } else if (req.method === "PATCH") {
-    //   try {
-    //     const { project_passed, notes } = req.body;
-    //     const patchProjectGrade = await sql`
-    //              UPDATE project_grades SET project_passed = ${project_passed}, notes = ${notes}
-    //              WHERE project_id = ${id}`;
-    //     res.status(200).json(patchProjectGrade);
-    //   } catch (error) {
-    //     console.error("Bad news in index api: ", error);
-    //     return res.status(500).json({ msg: "Messed up on our end" });
-    //   }
-    // } else if (req.method === "PATCH") {
-    //   try {
-    //     const { project_name } = req.body;
-    //     const patchProject = await sql`
-    //                UPDATE projects SET project_name = ${project_name}
-    //                WHERE project_id = ${id}`;
-    //     res.status(200).json(patchProject);
-    //   } catch (error) {
-    //     console.error("Bad news in index api: ", error);
-    //     return res.status(500).json({ msg: "Messed up on our end" });
-    //   }
+  } else if (req.method === "PATCH") {
+    try {
+      const { project_passed, notes } = req.body;
+      const patchProjectGrade = await sql`
+                   UPDATE project_grades SET project_passed = ${project_passed}, notes = ${notes}
+                   WHERE project_id = ${id}`;
+      res.status(200).json(patchProjectGrade);
+    } catch (error) {
+      console.error("Bad news in index api: ", error);
+      return res.status(500).json({ msg: "Messed up on our end" });
+    }
   } else {
     res.status(400).json({ msg: "You messed up" });
   }
