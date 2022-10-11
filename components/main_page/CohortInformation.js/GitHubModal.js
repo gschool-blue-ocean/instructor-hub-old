94c4f623483e697c3b44ea36757492bd39109f16
BlazingAces <1002charles@gmail.com>
2022-10-11 10:45:37 -0400

    Changes made before a pull

--- components/main_page/CohortInformation.js/GitHubModal.js
+++ components/main_page/CohortInformation.js/GitHubModal.js
@@ -1,62 +1,101 @@
 import { useState } from "react";
 import { useRecoilState } from "recoil";
 import { studentsState } from "../../state.js";
-import gitStyle from '../../../styles/GitHub.module.css'
-import Image from 'next/image'
+import gitStyle from "../../../styles/GitHub.module.css";
+import Image from "next/image";
 
 const GitHubModal = ({ showGitHubModal, setShowGitHubModal, onClose }) => {
   const [students, setStudents] = useRecoilState(studentsState);
-  
-    return (
-      <>
-    {showGitHubModal ? (
+  const [gitHubAccount, setGitHubAccount] = useRecoilState(studentsState);
+  // console.log(gitHubAccount[0].github);
+
+  // not complete
+  const addGitHubAccount = () => {
+    axios({
+      method: "post",
+      url: `/api/students`,
+      data: {
+        github: "",
+      },
+    });
+  };
+
+  return (
+    <>
+      {showGitHubModal ? (
         <div>
           <div className={gitStyle.background} onClick={onClose}></div>
-          <div className= {gitStyle.gitContainer}>
-            <div className = {gitStyle.top_bar}></div>
-              <div>
-                <div className= {gitStyle.listHeader}>
-                  <div>
-                    <div className= {gitStyle.headerInfo}>
-                      <div className= {gitStyle.info}><svg className= {gitStyle.personIcon}><path fillRule="evenodd" d="M10.5 5a2.5 2.5 0 11-5 0 2.5 2.5 0 015 0zm.061 3.073a4 4 0 10-5.123 0 6.004 6.004 0 00-3.431 5.142.75.75 0 001.498.07 4.5 4.5 0 018.99 0 .75.75 0 101.498-.07 6.005 6.005 0 00-3.432-5.142z"></path></svg> 2 members </div>
+          <div className={gitStyle.gitContainer}>
+            <div className={gitStyle.top_bar}></div>
+            <div>
+              <div className={gitStyle.listHeader}>
+                <div>
+                  <div className={gitStyle.headerInfo}>
+                    <div className={gitStyle.info}>
+                      <svg className={gitStyle.personIcon}>
+                        <path
+                          fillRule="evenodd"
+                          d="M10.5 5a2.5 2.5 0 11-5 0 2.5 2.5 0 015 0zm.061 3.073a4 4 0 10-5.123 0 6.004 6.004 0 00-3.431 5.142.75.75 0 001.498.07 4.5 4.5 0 018.99 0 .75.75 0 101.498-.07 6.005 6.005 0 00-3.432-5.142z"
+                        ></path>
+                      </svg>{" "}
+                      2 members{" "}
                     </div>
-                    <div onClick={onClose} className= {gitStyle.close}></div>
                   </div>
+                  <div onClick={onClose} className={gitStyle.close}></div>
                 </div>
-                <ul className= {gitStyle.tableList}>
-                  <li className= {gitStyle.tableListItem}>
-                    <div className= {gitStyle.tableListCell}>
-                      <span className= {gitStyle.frameLeft}>
-                        <a className= {gitStyle.frameInline} href= "#">
-                          <Image src= '/pic1.jpg' height= "44" width= "44" className= {gitStyle.avatar}/>
-                        </a>
-                      </span>
-                    </div>
-                    <div className= {gitStyle.tableListName}>
-                      <a className= {gitStyle.frameInclineName}> {students.name_first +" "+students.name_last} </a>
-                      <span className= {gitStyle.codeName}> {students.github} </span>
-                    </div>
-                  </li>
-                  <li className= {gitStyle.tableListItem}>
-                    <div className= {gitStyle.tableListCell}>
-                      <span className= {gitStyle.frameLeft}>
-                        <a className= {gitStyle.frameInline} href= "#">
-                          <Image src= '/pic1.jpg' height= "44" width= "44" className= {gitStyle.avatar}/>
-                        </a>
-                      </span>
-                    </div>
-                    <div className= {gitStyle.tableListName}>
-                      <a className= {gitStyle.frameInclineName}> Student 2 </a>
-                      <span className= {gitStyle.codeName}> BurtMFReynolds</span>
-                    </div>
-                  </li>
-                </ul> 
               </div>
+              <ul className={gitStyle.tableList}>
+              {students.map((student) => (
+                <li className={gitStyle.tableListItem}>
+                  <div className={gitStyle.tableListCell}>
+                    <span className={gitStyle.frameLeft}>
+                      <a className={gitStyle.frameInline} href="#">
+                        <Image
+                          src="/pic1.jpg"
+                          height="44"
+                          width="44"
+                          className={gitStyle.avatar}
+                        />
+                      </a>
+                    </span>
+                  </div>
+                  <div className={gitStyle.tableListName}>
+                    <a className={gitStyle.frameInclineName}>
+                      {" "}
+                      {student.name_first + " " + student.name_last}{" "}
+                    </a>
+                    <span className={gitStyle.codeName}>
+                      {" "}
+                      {student.github}{" "}
+                    </span>
+                  </div>
+                </li>
+                ))}
+                <li className={gitStyle.tableListItem}>
+                  <div className={gitStyle.tableListCell}>
+                    <span className={gitStyle.frameLeft}>
+                      <a className={gitStyle.frameInline} href="#">
+                        <Image
+                          src="/pic1.jpg"
+                          height="44"
+                          width="44"
+                          className={gitStyle.avatar}
+                        />
+                      </a>
+                    </span>
+                  </div>
+                  <div className={gitStyle.tableListName}>
+                    <a className={gitStyle.frameInclineName}> Student 2 </a>
+                    <span className={gitStyle.codeName}> BurtMFReynolds</span>
+                  </div>
+                </li>
+              </ul>
             </div>
           </div>
-        ) : null}
-      </>
-    );
-  };
-  
-  export default GitHubModal;
+        </div>
+      ) : null}
+    </>
+  );
+};
+
+export default GitHubModal;
--- components/main_page/CohortInformation.js/StudentSummary.js
+++ components/main_page/CohortInformation.js/StudentSummary.js
@@ -10,7 +10,20 @@ const StudentSummary = () => {
   const [showGitHubModal, setShowGitHubModal] = useState(false);
   const [showCommentModal, setShowCommenttModal] = useState(false);
   const [checkedAll, setCheckAll] = useState(false);
+  const [order, setOrder] = useState("ASC")
   // const [checked, setChecked] = useState({ students: false });
+
+  const sorting= (col) => {
+    if(order === 'ASC') {
+      const sorted = [...students].sort((a,b) =>
+        a[col] < b[col] ? 1 : -1
+      );
+      setStudents(sorted);
+      setOrder("ASC")
+    }
+  }
+
+  console.log(students)
 
   const openGitHubModal = () => {
     setShowGitHubModal((prev) => !prev);
@@ -23,6 +36,7 @@ const StudentSummary = () => {
   useEffect(() => {
     setStudents(students)
   }, []);
+
 
   const handleChange = (e) => {
     const { id, checked } = e.target;
@@ -39,6 +53,12 @@ const StudentSummary = () => {
     }
   }
 
+  const handleDeleteClick = (studentId) => {
+    const newStudent = [...students] //Create New Array based on current students
+    const index = students.findIndex((student) => student.id === studentId)
+    newStudent.splice(index, 1);
+    setStudents(newStudent)
+  }
 
   // onChange={() => toggleCheck("{student.student_id}")} checked={checked["{student.student_id}"]}
   // const toggleCheck = (inputName) => {
@@ -75,11 +95,22 @@ const StudentSummary = () => {
   // }, [checked]);
 
   return (
-
     <div>
-      <GitHubModal showGitHubModal={showGitHubModal} setShowGitHubModal={setShowGitHubModal} onClose={() => {setShowGitHubModal(false)}}/>
-      <CommentModal showCommentModal={showCommentModal} setShowCommenttModal={setShowGitHubModal} onClose={() => {setShowCommenttModal(false)}}/>
-      <div className= {studentStyle.container}>
+      <GitHubModal
+        showGitHubModal={showGitHubModal}
+        setShowGitHubModal={setShowGitHubModal}
+        onClose={() => {
+          setShowGitHubModal(false);
+        }}
+      />
+      <CommentModal
+        showCommentModal={showCommentModal}
+        setShowCommenttModal={setShowGitHubModal}
+        onClose={() => {
+          setShowCommenttModal(false);
+        }}
+      />
+      <div className={studentStyle.container}>
         <div className={studentStyle.topBorder}>
           <div className={studentStyle.selectRow}>
             <div className={studentStyle.selectAllBox}>
@@ -89,7 +120,9 @@ const StudentSummary = () => {
                 className={studentStyle.checkBox}
                 type="checkbox"
                 name="allSelect"
-                checked={!students.some((student) => student?.isChecked !== true)}
+                checked={
+                  !students.some((student) => student?.isChecked !== true)
+                }
                 onChange={handleChange}
               />
               <label htmlFor="selectMe"> Select/Deselect All</label>
@@ -108,25 +141,19 @@ const StudentSummary = () => {
         </div>
         <div className={studentStyle.middleBorder}>
           <div>
-            <table className= {studentStyle.table} border= "1">
+            <table className= {studentStyle.table}>
               <thead className= {studentStyle.thead}>
                 <tr className= {studentStyle.headerRow}>
                   <th className= {studentStyle.smallHeader}></th>
-                  <th className= {studentStyle.header} scope="col">
-                    <span>Name</span>
-                  </th>
-                  <th className= {studentStyle.header} scope="col">
-                    <span>Learn Average</span>
-                  </th>
-                  <th className= {studentStyle.header} scope="col">
+                  <th className= {studentStyle.header} scope="col" onClick={() => sorting("name")}>Name</th>
+                  <th className= {studentStyle.header} scope="col" onClick={() => sorting("Learn_Avg")}>Learn Avg</th>
+                  <th className= {studentStyle.header} scope="col"onClick={() => sorting("ClientSide")}>ClientSide</th>
+                  <th className= {studentStyle.header} scope="col" onClick={() => sorting("ServerSide")}>ServerSide</th>
+                  {/* <th className= {studentStyle.header} scope="col">
                     <span>Teamwork</span>
-                  </th>
-                  <th className= {studentStyle.header} scope="col">
-                    <span>Tech Skills</span>
-                  </th>
-                  <th className= {studentStyle.header} scope="col">
-                    <span>Comments</span>
-                  </th>
+                  </th> */}
+                  <th className= {studentStyle.header} scope="col" onClick={() => sorting("Tech_Skills")}>Tech Skills</th>
+                  <th className= {studentStyle.header} scope="col">Notes</th>
                   <th className= {studentStyle.smallHeader} scope="col"></th>
                 </tr>
               </thead>
@@ -136,11 +163,13 @@ const StudentSummary = () => {
                   <td className= {studentStyle.smallContent}>
                     <input type="checkbox" name={student.id} checked={student?.isChecked || false} onChange={handleChange}></input>
                   </td>
-                  <td  className= {studentStyle.content}>
-                    <a href="#">{student.name_first +" "+student.name_last}</a>
+                  <td  className= {studentStyle.nameContent}>
+                    <a className= {studentStyle.nameSpace} href="#">{student.name_first +" "+student.name_last}</a>
                   </td>
-                  <td className= {studentStyle.content} >{student.learn_avg}%</td>
-                  <td className= {studentStyle.content}>Terrible</td>
+                  <td className= {studentStyle.content}>{student.learn_avg}%</td>
+                  <td className= {studentStyle.content}>{student.client_side_test}</td>
+                  <td className= {studentStyle.content}>{student.server_side_test}</td>
+                  {/* <td className= {studentStyle.content}>{student.teamwork_avg}</td> */}
                   <td className= {studentStyle.content}>
                     <div className={studentStyle.color3}>At Risk</div>
                   </td>
@@ -163,77 +192,13 @@ const StudentSummary = () => {
                     </svg>
                   </td>
                   <td className= {studentStyle.smallContent}>
-                    <svg className={studentStyle.dot}>
+                    <svg className={studentStyle.trash} viewBox="0 0 12 12" onClick={()=> handleDeleteClick(student.student_id)}>
                       {" "}
-                      <path d="M8 9a1.5 1.5 0 100-3 1.5 1.5 0 000 3zM1.5 9a1.5 1.5 0 100-3 1.5 1.5 0 000 3zm13 0a1.5 1.5 0 100-3 1.5 1.5 0 000 3z"></path>{" "}
+                      <path d="M6.5 17q-.625 0-1.062-.438Q5 16.125 5 15.5v-10H4V4h4V3h4v1h4v1.5h-1v10q0 .625-.438 1.062Q14.125 17 13.5 17Zm7-11.5h-7v10h7ZM8 14h1.5V7H8Zm2.5 0H12V7h-1.5Zm-4-8.5v10Z"></path>{" "}
                     </svg>{" "}
                   </td>
                 </tr>
                 ))}
-                {/* <tr className={studentStyle.tbodyRow}>
-                  <td>
-                    <input
-                      type="checkbox"
-                      name="name"
-                      // onChange={() => toggleCheck("row2")}
-                      // checked={checked["row2"]}
-                    ></input>
-                  </td>
-                  <td className={studentStyle.content}>
-                    <a href="#">Student 2</a>
-                  </td>
-                  <td className={studentStyle.content}>70/100</td>
-                  <td className={studentStyle.content}>
-                    <div className={studentStyle.color2}>On Track</div>
-                  </td>
-                  <td
-                    onClick={openCommentModel}
-                    className={studentStyle.content}
-                  >
-                    {" "}
-                    <svg className={studentStyle.noteIcon} viewBox="0 0 22 22">
-                      {" "}
-                      <path d="M13.5,20 C14.3284271,20 15,19.3284271 15,18.5 C15,17.1192881 16.1192881,16 17.5,16 C18.3284271,16 19,15.3284271 19,14.5 L19,11.5 C19,11.2238576 19.2238576,11 19.5,11 C19.7761424,11 20,11.2238576 20,11.5 L20,14.5 C20,18.0898509 17.0898509,21 13.5,21 L6.5,21 C5.11928813,21 4,19.8807119 4,18.5 L4,5.5 C4,4.11928813 5.11928813,3 6.5,3 L12.5,3 C12.7761424,3 13,3.22385763 13,3.5 C13,3.77614237 12.7761424,4 12.5,4 L6.5,4 C5.67157288,4 5,4.67157288 5,5.5 L5,18.5 C5,19.3284271 5.67157288,20 6.5,20 L13.5,20 L13.5,20 Z M15.7913481,19.5014408 C16.9873685,18.9526013 17.9526013,17.9873685 18.5014408,16.7913481 C18.1948298,16.9255432 17.8561101,17 17.5,17 C16.6715729,17 16,17.6715729 16,18.5 C16,18.8561101 15.9255432,19.1948298 15.7913481,19.5014408 L15.7913481,19.5014408 Z M18,6 L20.5,6 C20.7761424,6 21,6.22385763 21,6.5 C21,6.77614237 20.7761424,7 20.5,7 L18,7 L18,9.5 C18,9.77614237 17.7761424,10 17.5,10 C17.2238576,10 17,9.77614237 17,9.5 L17,7 L14.5,7 C14.2238576,7 14,6.77614237 14,6.5 C14,6.22385763 14.2238576,6 14.5,6 L17,6 L17,3.5 C17,3.22385763 17.2238576,3 17.5,3 C17.7761424,3 18,3.22385763 18,3.5 L18,6 Z M8.5,9 C8.22385763,9 8,8.77614237 8,8.5 C8,8.22385763 8.22385763,8 8.5,8 L12.5,8 C12.7761424,8 13,8.22385763 13,8.5 C13,8.77614237 12.7761424,9 12.5,9 L8.5,9 Z M8.5,12 C8.22385763,12 8,11.7761424 8,11.5 C8,11.2238576 8.22385763,11 8.5,11 L15.5,11 C15.7761424,11 16,11.2238576 16,11.5 C16,11.7761424 15.7761424,12 15.5,12 L8.5,12 Z M8.5,15 C8.22385763,15 8,14.7761424 8,14.5 C8,14.2238576 8.22385763,14 8.5,14 L13.5,14 C13.7761424,14 14,14.2238576 14,14.5 C14,14.7761424 13.7761424,15 13.5,15 L8.5,15 Z"></path>
-                    </svg>
-                  </td>
-                  <td>
-                    <svg className={studentStyle.dot}>
-                      {" "}
-                      <path d="M8 9a1.5 1.5 0 100-3 1.5 1.5 0 000 3zM1.5 9a1.5 1.5 0 100-3 1.5 1.5 0 000 3zm13 0a1.5 1.5 0 100-3 1.5 1.5 0 000 3z"></path>
-                    </svg>{" "}
-                  </td>
-                </tr> */}
-
-                {/* <tr className={studentStyle.tbodyRow}>
-                  <td>
-                    <input
-                      type="checkbox"
-                      name="name"
-                      // onChange={() => toggleCheck("row3")}
-                      // checked={checked["row3"]}
-                    ></input>
-                  </td>
-                  <td className={studentStyle.content}>
-                    <a href="#">Student 3</a>
-                  </td>
-                  <td className={studentStyle.content}>95/100</td>
-                  <td className={studentStyle.content}>
-                    <div className={studentStyle.color2}>On Track</div>
-                  </td>
-                  <td className={studentStyle.content}>
-                    {" "}
-                    <svg className={studentStyle.noteIcon} viewBox="0 0 22 22">
-                      {" "}
-                      <path d="M13.5,20 C14.3284271,20 15,19.3284271 15,18.5 C15,17.1192881 16.1192881,16 17.5,16 C18.3284271,16 19,15.3284271 19,14.5 L19,11.5 C19,11.2238576 19.2238576,11 19.5,11 C19.7761424,11 20,11.2238576 20,11.5 L20,14.5 C20,18.0898509 17.0898509,21 13.5,21 L6.5,21 C5.11928813,21 4,19.8807119 4,18.5 L4,5.5 C4,4.11928813 5.11928813,3 6.5,3 L12.5,3 C12.7761424,3 13,3.22385763 13,3.5 C13,3.77614237 12.7761424,4 12.5,4 L6.5,4 C5.67157288,4 5,4.67157288 5,5.5 L5,18.5 C5,19.3284271 5.67157288,20 6.5,20 L13.5,20 L13.5,20 Z M15.7913481,19.5014408 C16.9873685,18.9526013 17.9526013,17.9873685 18.5014408,16.7913481 C18.1948298,16.9255432 17.8561101,17 17.5,17 C16.6715729,17 16,17.6715729 16,18.5 C16,18.8561101 15.9255432,19.1948298 15.7913481,19.5014408 L15.7913481,19.5014408 Z M18,6 L20.5,6 C20.7761424,6 21,6.22385763 21,6.5 C21,6.77614237 20.7761424,7 20.5,7 L18,7 L18,9.5 C18,9.77614237 17.7761424,10 17.5,10 C17.2238576,10 17,9.77614237 17,9.5 L17,7 L14.5,7 C14.2238576,7 14,6.77614237 14,6.5 C14,6.22385763 14.2238576,6 14.5,6 L17,6 L17,3.5 C17,3.22385763 17.2238576,3 17.5,3 C17.7761424,3 18,3.22385763 18,3.5 L18,6 Z M8.5,9 C8.22385763,9 8,8.77614237 8,8.5 C8,8.22385763 8.22385763,8 8.5,8 L12.5,8 C12.7761424,8 13,8.22385763 13,8.5 C13,8.77614237 12.7761424,9 12.5,9 L8.5,9 Z M8.5,12 C8.22385763,12 8,11.7761424 8,11.5 C8,11.2238576 8.22385763,11 8.5,11 L15.5,11 C15.7761424,11 16,11.2238576 16,11.5 C16,11.7761424 15.7761424,12 15.5,12 L8.5,12 Z M8.5,15 C8.22385763,15 8,14.7761424 8,14.5 C8,14.2238576 8.22385763,14 8.5,14 L13.5,14 C13.7761424,14 14,14.2238576 14,14.5 C14,14.7761424 13.7761424,15 13.5,15 L8.5,15 Z"></path>
-                    </svg>
-                  </td>
-                  <td>
-                    <svg className={studentStyle.dot}>
-                      {" "}
-                      <path d="M8 9a1.5 1.5 0 100-3 1.5 1.5 0 000 3zM1.5 9a1.5 1.5 0 100-3 1.5 1.5 0 000 3zm13 0a1.5 1.5 0 100-3 1.5 1.5 0 000 3z"></path>{" "}
-                    </svg>{" "}
-                  </td>
-                </tr>  */}
               </tbody>
             </table>
           </div>
