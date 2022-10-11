a3a93dc0c60ad5881179e164b22cb25922238a1c
Zach Hudson <hudsonzachpro@gmail.com>
2022-10-11 17:36:30 -0600

    Why won't my stupid code read the stupid state properly

--- components/main_page/CohortInformation.js/UpdateModal.js
+++ components/main_page/CohortInformation.js/UpdateModal.js
@@ -4,11 +4,11 @@ import { usersState, studentsState } from "../../state";
 import styles from "../../../styles/UpdateModal.module.css";
 
 const UpdateModal = ({ showUpdateModal, setShowUpdateModal, onClose }) => {
+  const [currStudent, setCurrStudent] = useState(0);
   const [modal, setModal] = useState(false);
   const [stagedCohort, setStagedCohort] = useState({});
   const [user, setUser] = useRecoilState(usersState);
   const [students, setStudents] = useRecoilState(studentsState);
-  const [currStudent, setCurrStudent] = useState(0);
 
   // This will likely be replaced by some value grabbed from state/Recoil.
   // const cohort = {};
@@ -39,8 +39,6 @@ const UpdateModal = ({ showUpdateModal, setShowUpdateModal, onClose }) => {
     let stagedStudent = { [stagedName]: {} };
     let formData = new FormData(form);
     for (const pair of formData.entries()) {
-      console.log(stagedStudent);
-      console.log(pair);
       stagedStudent[stagedName][pair[0]] = pair[1];
     }
 
@@ -53,6 +51,7 @@ const UpdateModal = ({ showUpdateModal, setShowUpdateModal, onClose }) => {
         currStudent = 0;
       }
     });
+
     console.log("New currStudent is number: ", currStudent);
     return stagedStudent;
   };
@@ -65,62 +64,73 @@ const UpdateModal = ({ showUpdateModal, setShowUpdateModal, onClose }) => {
 
           <div className={styles.UpdateModal}>
             <div className={styles.header}>
-              Update - {students[currStudent].name_first || "Null"}
+              Update - {"Dude being edited"}
               <button className={styles.button} onClick={onClose}>
                 X
               </button>
             </div>
             <div className={styles.update}>
-              <form
-                className={styles.updateForm}
-                onSubmit={submitHandler}
-                onKeyDown={enterListener}
-              >
-                <label htmlFor="Tech">Technical Aptitude</label> <br />
-                <select id="Tech" name="Tech" required autoFocus="true">
-                  <option value="none" selected disabled hidden>
-                    Select an Option
-                  </option>
-                  <option value="1 - Needs improvement">
-                    1 - Needs improvement
-                  </option>
-                  <option value="2 - Approaching standard">
-                    2 - Approaching standard
-                  </option>
-                  <option value="3 - Meets standard">3 - Meets standard</option>
-                  <option value="4 - Exceeds standard">
-                    4 - Exceeds standard
-                  </option>
-                </select>{" "}
-                <br />
-                <label htmlFor="Team">Teamwork Aptitude</label> <br />
-                <select id="Team" name="Team" required>
-                  <option value="none" selected disabled hidden>
-                    Select an Option
-                  </option>
-                  <option value="1 - Needs improvement">
-                    1 - Needs improvement
-                  </option>
-                  <option value="2 - Approaching standard">
-                    2 - Approaching standard
-                  </option>
-                  <option value="3 - Meets standard">3 - Meets standard</option>
-                  <option value="4 - Exceeds standard">
-                    4 - Exceeds standard
-                  </option>
-                </select>{" "}
-                <br />
-                <label htmlFor="Notes">Notes</label> <br />
-                <textarea
-                  id="Notes"
-                  name="Notes"
-                  rows="10"
-                  cols="30"
-                  required
-                ></textarea>{" "}
-                <br />
-                <input type="submit" value="Submit" />
-              </form>
+              {currStudent < length ? (
+                <form
+                  className={styles.updateForm}
+                  onSubmit={submitHandler}
+                  onKeyDown={enterListener}
+                >
+                  <label htmlFor="Tech">Technical Aptitude</label> <br />
+                  <select id="Tech" name="Tech" required autoFocus={true}>
+                    <option value="none" selected disabled hidden>
+                      Select an Option
+                    </option>
+                    <option value="1 - Needs improvement">
+                      1 - Needs improvement
+                    </option>
+                    <option value="2 - Approaching standard">
+                      2 - Approaching standard
+                    </option>
+                    <option value="3 - Meets standard">
+                      3 - Meets standard
+                    </option>
+                    <option value="4 - Exceeds standard">
+                      4 - Exceeds standard
+                    </option>
+                  </select>{" "}
+                  <br />
+                  <label htmlFor="Team">Teamwork Aptitude</label> <br />
+                  <select id="Team" name="Team" required>
+                    <option value="none" selected disabled hidden>
+                      Select an Option
+                    </option>
+                    <option value="1 - Needs improvement">
+                      1 - Needs improvement
+                    </option>
+                    <option value="2 - Approaching standard">
+                      2 - Approaching standard
+                    </option>
+                    <option value="3 - Meets standard">
+                      3 - Meets standard
+                    </option>
+                    <option value="4 - Exceeds standard">
+                      4 - Exceeds standard
+                    </option>
+                  </select>{" "}
+                  <br />
+                  <label htmlFor="Notes">Notes</label> <br />
+                  <textarea
+                    id="Notes"
+                    name="Notes"
+                    rows="10"
+                    cols="30"
+                    required
+                  ></textarea>{" "}
+                  <br />
+                  <input type="submit" value="Submit" />
+                </form>
+              ) : (
+                <span>
+                  You have graded all your students, now you can relax and go
+                  code with your buds.
+                </span>
+              )}
             </div>
           </div>
         </>
