9433994a3cba8afe4df6727330332447b74c1879
SamChav <chavez.samuel1@gmail.com>
2022-10-11 15:13:58 -0700

    user api created and using user data in page load for first time

--- components/main_page/CohortInformation.js/UpdateModal.js
+++ components/main_page/CohortInformation.js/UpdateModal.js
@@ -1,14 +1,15 @@
 import axios from "axios";
 import { useEffect, useState } from "react";
 import styles from "../../../styles/UpdateModal.module.css";
-
+import { useRecoilState } from "recoil";
+import { usersState } from "../../state";
 const UpdateModal = ({ showUpdateModal, setShowUpdateModal, onClose }) => {
   const [modal, setModal] = useState(false);
   const [updatedInfo, setUpdatedInfo] = useState({})
   const [techSkillGid, setTechSkillGid] = useState(null)
   const [teamWorkGid, setTeamWorkGid] = useState(null)
   const [studentList, setStudentList] = useState([])
-
+  const [users, setUsers] = useRecoilState(usersState)
 
 
   // This will likely be replaced by some value grabbed from state/Recoil.
@@ -17,39 +18,38 @@ const UpdateModal = ({ showUpdateModal, setShowUpdateModal, onClose }) => {
   // In addition, it will be necessary to grab
   // "current student" from state.
   const fetchData = async () => {
-    axios.get("https://app.asana.com/api/1.0/projects/1203082294663367" , {
+    axios.get(`https://app.asana.com/api/1.0/projects/${users.cohort_asana_gid}` , {
       headers: {
-        Authorization: "Bearer 1/1202490391764279:bea7f8d535303444d7b1aa5e74101eec",
+        Authorization: `Bearer ${users.asana_access_token}`,
       }
     })
     .then((res) => {
       setTechSkillGid(res.data.data.custom_field_settings[0].custom_field.gid)
       setTeamWorkGid(res.data.data.custom_field_settings[1].custom_field.gid)
     })
-    axios.get("https://app.asana.com/api/1.0/tasks/?project=1203082294663367", {
+    axios.get(`https://app.asana.com/api/1.0/tasks/?project=${users.cohort_asana_gid}`, {   // user cohort
       headers: {
-        Authorization: "Bearer 1/1202490391764279:bea7f8d535303444d7b1aa5e74101eec",
+        Authorization: `Bearer ${users.asana_access_token}`,
       }
     })
     .then((res) => {
       setStudentList(res.data.data)
     })
+    .then(() => {      
+      axios.get("/api/students")
+      .then((res) => {
+        console.log(res, "student data from api")
+      })
+    })
+    // axios.get(`/api/users/${id}`)
+    // .then((res) => {
+    //   console.log(res, "user get response")
+    // })
 
-<<<<<<< HEAD
   }
-=======
-  const submitHandler = (e) => {
-    e.preventDefault();
 
-    console.log("What is the submit 'e'?", e);
-    const stagedStudent = formGetter(e.target);
-    setStagedCohort((prev) => ({
-      ...stagedStudent,
-    }));
-  };
->>>>>>> 30aefecd22141cc792110e402b8de28afac80cdd
-
-  useEffect(() => { 
+  useEffect(() => {
+    console.log(users, "user data on load")
     fetchData();
 }, [])
 
@@ -66,7 +66,7 @@ const UpdateModal = ({ showUpdateModal, setShowUpdateModal, onClose }) => {
     let instructorNotes = ''
     await axios.get("https://app.asana.com/api/1.0/tasks/1203111662850732", {
       headers: {
-        Authorization: "Bearer 1/1202490391764279:bea7f8d535303444d7b1aa5e74101eec",
+        Authorization: `Bearer ${users.asana_access_token}`,  //will get logged in user's asana token
       }
     })
     .then((res) => {
@@ -80,13 +80,13 @@ const studentGid = studentList[0].gid //<<----LOCAL DB CALL
         method:"PUT",  //must be put method not patch
         url: `https://app.asana.com/api/1.0/tasks/${studentGid}`, //need task id variable -- sooo...this student gid needs to be filled when the student is selected, need to correlate between this LOCAL DB NEEDED
         headers: {
-          Authorization: "Bearer 1/1202490391764279:bea7f8d535303444d7b1aa5e74101eec",  //need template literal for ALLLLL headers so global state dependant on user
+          Authorization: `Bearer ${users.asana_access_token}`,  //need template literal for ALLLLL headers so global state dependant on user
         }, 
           data: { 
             data: {
               "workspace": "1213745087037",
               "assignee_section": null,
-              "html_notes": `<body>${instructorNotes}\n ${cohort.studentInfo.assessmentName.toUpperCase()} ${cohort.studentInfo.assessmentScore}</body>`, //need conditional or neeed to make this field mandatory
+              // "html_notes": `<body>${instructorNotes}\n ${cohort.studentInfo.assessmentName.toUpperCase()}: ${cohort.studentInfo.assessmentScore}</body>`, //need conditional or neeed to make this field mandatory, move to grades button
               "parent": null,
               "resource_subtype": "default_task",
               "custom_fields": {
@@ -100,7 +100,7 @@ const studentGid = studentList[0].gid //<<----LOCAL DB CALL
       method:"POST",  //must be put method not patch
         url: `https://app.asana.com/api/1.0/tasks/${studentGid}/subtasks`, //need task id variable -- sooo...this student gid needs to be filled when the student is selected, need to correlate between this
         headers: {
-          Authorization: "Bearer 1/1202490391764279:bea7f8d535303444d7b1aa5e74101eec",  //need template literal for ALLLLL headers so global state dependant on user <<---test next week with all members
+          Authorization: `Bearer ${users.asana_access_token}`,  //need template literal for ALLLLL headers so global state dependant on user <<---test next week with all members
         }, 
         data: { 
           data: {            
@@ -181,14 +181,6 @@ const studentGid = studentList[0].gid //<<----LOCAL DB CALL
                     4 - Exceeds standard
                   </option>
                 </select>{" "}
-                <br />
-                <label htmlFor="assessmentName">Assement Name:</label>
-                <br />
-                <input name="assessmentName" placeholder="Enter assessment name"></input>
-                <br />
-                <label htmlFor="assessmentScore">Score:</label>
-                <br />
-                <input name="assessmentScore" placeholder="Enter score"></input>
                 <br />
                 <label htmlFor="Notes">Notes</label> <br />
                 <textarea
