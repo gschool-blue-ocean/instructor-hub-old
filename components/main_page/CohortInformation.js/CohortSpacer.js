7fb9be5fae3e68a54af8ba0184adbd0dba3c6a81
Gerard San Juan <49268224+Tiwala@users.noreply.github.com>
2022-10-12 18:26:03 -0500

    made a sync cohorts button where the objective is to make a get request for the cohorts on asana, and post cohorts that are not in our state to the local database. POST REQUEST RETURNS 500 ERROR

--- components/main_page/CohortInformation.js/CohortSpacer.js
+++ components/main_page/CohortInformation.js/CohortSpacer.js
@@ -1,14 +1,16 @@
 import spacerStyle from '../../../styles/CohortSpacer.module.css'
 import { useEffect, useState } from 'react'
-import NewCohortModal from './NewCohortModal'
+// import NewCohortModal from './NewCohortModal'
 import { useRecoilState } from "recoil";
-import { cohortsState, currentCohortState, usersState } from "../../state.js";
+import { cohortsState, currentCohortState, usersState, accessToken } from "../../state.js";
+import axios from 'axios'
 
 const CohortSpacer = () => {
-  const [newCohortModal, showNewCohortModal] = useState(false)
+  // const [newCohortModal, showNewCohortModal] = useState(false)
   const [cohorts, setCohorts] = useRecoilState(cohortsState);
   const [currentCohort, setCurrentCohort] = useRecoilState(currentCohortState)
   const [user, setUser] = useRecoilState(usersState)
+  // const [asana_access_token, setAsana_Access_Token] = useRecoilState(accessToken)
 
   useEffect(() => {
     if (user) {
@@ -16,6 +18,9 @@ const CohortSpacer = () => {
     }
   }, [])
 
+  useEffect(() => {
+    console.log(cohorts)
+  }, [cohorts])
 
   useEffect(() => {
     // When currentCohort changes, i.e. via the select cohort feature, logs the current cohort name and object
@@ -23,15 +28,39 @@ const CohortSpacer = () => {
     console.log(cohorts.filter(current => current.name === currentCohort)[0])
   }, [currentCohort])
 
-  const newCohort = () => {
-    showNewCohortModal(!newCohortModal)
+  const syncCohorts = () => {
+    console.log(cohorts)
+    axios.get('https://app.asana.com/api/1.0/projects/', {
+      headers: {
+          Authorization: `Bearer ${user.asana_access_token}`,
+        },
+    }).then((res) => {
+        let asanaCohorts = res.data.data
+        console.log(asanaCohorts)
+        // WORK IN PROGRESS, POST REQUEST DOES NOT WORK
+        // for (let cohort of asanaCohorts) {
+          // if (cohorts.filter(current => current.gid !== cohort.gid)) {
+            axios.post('/api/cohorts', {
+              // "name": "blahblah",
+              // "begin_date": "",
+              // "end_date": "",
+              // "instructor": "",
+              // "cohort_avg": null,
+              // "cohort_min": null,
+              // "cohort_max": null,
+              // "gid": "69420"
+            })
+          // }
+        // }
+    })
+
   }
 
   return (
     <>
       <div className = {spacerStyle.spacerContainer}>
         <a className = {spacerStyle.gitBtn}>
-          <span onClick={newCohort} className ={` ${spacerStyle.gitBtn_medium} ${spacerStyle.span}`}>Add Cohort</span>
+          <span onClick={syncCohorts} className ={` ${spacerStyle.gitBtn_medium} ${spacerStyle.span}`}>Sync Cohorts</span>
         </a>
         <select id='select' className={spacerStyle.cohort} type='select' name='cohort' value={currentCohort} onChange={(e) => setCurrentCohort(e.target.value)}>
           <option value={currentCohort} selected>{currentCohort}</option>
@@ -40,12 +69,12 @@ const CohortSpacer = () => {
           ))}
         </select>
       </div>
-      <NewCohortModal
+      {/* <NewCohortModal
       showNewCohortModal={showNewCohortModal}
       newCohortModal={newCohortModal}
       onClose={() => {
         showNewCohortModal(false);
-      }}/>
+      }}/> */}
     </>
   )
 }
