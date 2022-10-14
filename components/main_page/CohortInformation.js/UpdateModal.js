9435f01bfcaade10e906070fc5ddf5419e6598ba
Zach Hudson <hudsonzachpro@gmail.com>
2022-10-14 08:39:16 -0600

    Some styling changes in UpdateModal. Next steps are stopping the Updater at end of cohort list and making the POST

--- components/main_page/CohortInformation.js/UpdateModal.js
+++ components/main_page/CohortInformation.js/UpdateModal.js
@@ -94,68 +94,79 @@ const UpdateModal = ({ showUpdateModal, setShowUpdateModal, onClose }) => {
 
           <div className={styles.UpdateModal}>
             <div className={styles.header}>
-              Update - {indexedStudent.name}
+              Update -{" "}
+              {course[currStudent]
+                ? indexedStudent.name
+                : "Weekly Update COMPLETE"}
               <button className={styles.button} onClick={onClose}>
                 X
               </button>
             </div>
             <div className={styles.update}>
-              <form
-                className={styles.updateForm}
-                onSubmit={submitHandler}
-                onKeyDown={enterListener}
-              >
-                <label htmlFor="Tech">Technical Aptitude</label> <br />
-                <select
-                  id="Tech"
-                  name="Tech"
-                  required
-                  autoFocus={true}
-                  ref={firstInput}
+              {course[currStudent] ? (
+                <form
+                  className={styles.updateForm}
+                  onSubmit={submitHandler}
+                  onKeyDown={enterListener}
                 >
-                  <option value="none" selected disabled hidden>
-                    Select an Option
-                  </option>
-                  <option value="1 - Needs improvement">
-                    1 - Needs improvement
-                  </option>
-                  <option value="2 - Approaching standard">
-                    2 - Approaching standard
-                  </option>
-                  <option value="3 - Meets standard">3 - Meets standard</option>
-                  <option value="4 - Exceeds standard">
-                    4 - Exceeds standard
-                  </option>
-                </select>{" "}
-                <br />
-                <label htmlFor="Team">Teamwork Aptitude</label> <br />
-                <select id="Team" name="Team" required>
-                  <option value="none" selected disabled hidden>
-                    Select an Option
-                  </option>
-                  <option value="1 - Needs improvement">
-                    1 - Needs improvement
-                  </option>
-                  <option value="2 - Approaching standard">
-                    2 - Approaching standard
-                  </option>
-                  <option value="3 - Meets standard">3 - Meets standard</option>
-                  <option value="4 - Exceeds standard">
-                    4 - Exceeds standard
-                  </option>
-                </select>{" "}
-                <br />
-                <label htmlFor="Notes">Notes</label> <br />
-                <textarea
-                  id="Notes"
-                  name="Notes"
-                  rows="10"
-                  cols="30"
-                  required
-                ></textarea>{" "}
-                <br />
-                <input type="submit" value="Submit" />
-              </form>
+                  <label htmlFor="Tech">Technical Aptitude</label> <br />
+                  <select
+                    id="Tech"
+                    name="Tech"
+                    required
+                    autoFocus={true}
+                    ref={firstInput}
+                  >
+                    <option value="none" selected disabled hidden>
+                      Select an Option
+                    </option>
+                    <option value="1 - Needs improvement">
+                      1 - Needs improvement
+                    </option>
+                    <option value="2 - Approaching standard">
+                      2 - Approaching standard
+                    </option>
+                    <option value="3 - Meets standard">
+                      3 - Meets standard
+                    </option>
+                    <option value="4 - Exceeds standard">
+                      4 - Exceeds standard
+                    </option>
+                  </select>{" "}
+                  <br />
+                  <label htmlFor="Team">Teamwork Aptitude</label> <br />
+                  <select id="Team" name="Team" required>
+                    <option value="none" selected disabled hidden>
+                      Select an Option
+                    </option>
+                    <option value="1 - Needs improvement">
+                      1 - Needs improvement
+                    </option>
+                    <option value="2 - Approaching standard">
+                      2 - Approaching standard
+                    </option>
+                    <option value="3 - Meets standard">
+                      3 - Meets standard
+                    </option>
+                    <option value="4 - Exceeds standard">
+                      4 - Exceeds standard
+                    </option>
+                  </select>{" "}
+                  <br />
+                  <label htmlFor="Notes">Notes</label> <br />
+                  <textarea
+                    id="Notes"
+                    name="Notes"
+                    rows="10"
+                    cols="30"
+                    required
+                  ></textarea>{" "}
+                  <br />
+                  <input type="submit" value="Submit" />
+                </form>
+              ) : (
+                <span>Go code with your buds, you're done</span>
+              )}
             </div>
           </div>
         </>
