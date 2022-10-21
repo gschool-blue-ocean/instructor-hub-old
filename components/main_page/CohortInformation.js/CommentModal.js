d2e40538b67f97076ce208ef20cf758c6da0122b
BlazingAces <1002charles@gmail.com>
2022-10-20 21:27:03 -0400

    update before a pull

--- components/main_page/CohortInformation.js/CommentModal.js
+++ components/main_page/CohortInformation.js/CommentModal.js
@@ -35,9 +35,9 @@ const CommentModal = ({ studentNote, showCommentModal, setShowCommenttModal, onC
                     <div className={commentStyle.headerContainer}>
                       <div className={commentStyle.headerLayout}>
                         <div className={commentStyle.textColor}>
-                          <div className={commentStyle.inlineBlock}>
-                            <h4 className={commentStyle.userName}>{user.username}</h4>
-                          </div>
+                          {/* <div className={commentStyle.inlineBlock}> */}
+                            {/* <h4 className={commentStyle.userName}>{user.username}</h4> */}
+                          {/* </div> */}
                           <>
                             <span className={commentStyle.time}> {new Date(note.note_date).toDateString()}</span>
                             <p className={commentStyle.commentSection}> {note.notes}</p>
--- pages/about.js
+++ pages/about.js
@@ -22,8 +22,12 @@ const about = () => {
               this is alot more difficult then I thought 
             </div>
             <div className={style.socialLink}>
-              <a className={style.links} href='#'><i>GH Icon</i></a>
-              <a className={style.links} href='#'><i>LI Icon</i></a>
+              <a className={style.links} href='https://github.com/A-Cobble'>
+                <Image alt="GitHub Icon" src='/GitHubIcon.png' width={48} height={48}></Image>
+              </a>
+              <a className={style.links} href='https://www.linkedin.com/in/alexandercobble/'>
+                <Image alt="Linkedin Icon" src='/LinkedinIcon.png' width={48} height={48}></Image>
+              </a>
             </div>
           </div>
           <div className={style.team}>
@@ -35,10 +39,10 @@ const about = () => {
             </div>
             <div className={style.socialLink}>
               <a className={style.links} href='https://github.com/Self-Zachtualization'>
-                <Image src='/GitHubIcon.png' width={48} height={48}></Image>
+                <Image alt="GitHub Icon" src='/GitHubIcon.png' width={48} height={48}></Image>
               </a>
-              <a className={style.links} href='#'>
-                <Image src='/LinkedinIcon.png' width={48} height={48}></Image>
+              <a className={style.links} href='https://www.linkedin.com/in/zacharychudson/'>
+                <Image alt="Linkedin Icon" src='/LinkedinIcon.png' width={48} height={48}></Image>
               </a>
             </div>
           </div>
@@ -46,15 +50,15 @@ const about = () => {
             <Image src='/Gerard.jpg' height={200} width={200}/>
 
             <div className={style.name}>Gerard San Juan</div>
-            <div className={style.about} >
-            Hi, I'm Gerard. Food is good, anime is wonderful, and software engineering is the way. 
+            <div className={style.about}>
+            {"Hi, I'm Gerard. Food is good, anime is wonderful, and software engineering is the way."} 
             </div>
             <div className={style.socialLink}>
               <a className={style.links} href='https://github.com/Tiwala'>
-                <Image src='/GitHubIcon.png' width={48} height={48}></Image>
+                <Image alt="GitHub Icon" src='/GitHubIcon.png' width={48} height={48}></Image>
               </a>
               <a className={style.links} href='https://www.linkedin.com/in/gerard-san-juan-ab8554162/'>
-                <Image src='/LinkedinIcon.png' width={48} height={48}></Image>
+                <Image alt="Linkedin Icon"src='/LinkedinIcon.png' width={48} height={48}></Image>
               </a>
             </div>
           </div>
@@ -67,8 +71,12 @@ const about = () => {
               this is alot more difficult then I thought 
             </div>
             <div className={style.socialLink}>
-              <a className={style.links} href='#'><i>GH Icon</i></a>
-              <a className={style.links} href='#'><i>LI Icon</i></a>
+              <a className={style.links} href='https://github.com/heymj317'>
+                <Image alt="GitHub Icon" src='/GitHubIcon.png' width={48} height={48}></Image>
+              </a>
+              <a className={style.links} href='https://www.linkedin.com/in/--michaeljefferson/'>
+                <Image alt="Linkedin Icon" src='/LinkedinIcon.png' width={48} height={48}></Image>
+              </a>
             </div>
           </div>
           <div className={style.team}>
@@ -80,21 +88,28 @@ const about = () => {
               this is alot more difficult then I thought 
             </div>
             <div className={style.socialLink}>
-              <a className={style.links} href='#'><i>GH Icon</i></a>
-              <a className={style.links} href='#'><i>LI Icon</i></a>
+              <a className={style.links} href='https://github.com/daryle-tan'>
+                <Image alt="GitHub Icon" src='/GitHubIcon.png' width={48} height={48}></Image>
+              </a>
+              <a className={style.links} href='https://www.linkedin.com/in/daryle-tan/'>
+                <Image alt="Linkedin Icon" src='/LinkedinIcon.png' width={48} height={48}></Image>
+              </a>
             </div>
           </div>
           <div className={style.team}>
-            <Image src='/pic1.jpg' height={200} width={200}/>
+            <Image src='/Charles.jpg' height={200} width={200}/>
 
             <div className={style.name}>Charles Vitanza</div>
             <div className={style.about} >
-              This is about me blah blah blah and more blah here, creative blahs only allowed or no, 
-              this is alot more difficult then I thought 
+              {"I am an inspiring software engineer who recently transitioned from the Air Force and dreams of eventually working as a software engineer in the video game industry"} 
             </div>
             <div className={style.socialLink}>
-              <a className={style.links} href='#'><i>GH Icon</i></a>
-              <a className={style.links} href='#'><i>LI Icon</i></a>
+              <a className={style.links} href='https://github.com/BlazingAces'>
+                <Image alt="GitHub Icon" src='/GitHubIcon.png' width={48} height={48}></Image>
+              </a>
+              <a className={style.links} href='https://www.linkedin.com/in/charles-a-vitanza/'>
+                <Image alt="Linkedin Icon" src='/LinkedinIcon.png' width={48} height={48}></Image>
+              </a>
             </div>
           </div>
           <div className={style.team}>
@@ -105,8 +120,12 @@ const about = () => {
               this is alot more difficult then I thought 
             </div>
             <div className={style.socialLink}>
-              <a className={style.links} href='#'><i>GH Icon</i></a>
-              <a className={style.links} href='#'><i>LI Icon</i></a>
+              <a className={style.links} href='https://github.com/mbcohen777'>
+                <Image alt="GitHub Icon" src='/GitHubIcon.png' width={48} height={48}></Image>
+              </a>
+              <a className={style.links} href='https://github.com/SamChav'>
+                <Image alt="Linkedin Icon" src='/LinkedinIcon.png' width={48} height={48}></Image>
+              </a>
             </div>
           </div>
           <div className={style.team}>
@@ -118,8 +137,12 @@ const about = () => {
               this is alot more difficult then I thought 
             </div>
             <div className={style.socialLink}>
-              <a className={style.links} href='#'><i>GH Icon</i></a>
-              <a className={style.links} href='#'><i>LI Icon</i></a>
+              <a className={style.links} href='https://github.com/SamChav'>
+                <Image alt="GitHub Icon" src='/GitHubIcon.png' width={48} height={48}></Image>
+              </a>
+              <a className={style.links} href='https://www.linkedin.com/in/samuel-i-chavez/'>
+                <Image alt="Linkedin Icon" src='/LinkedinIcon.png' width={48} height={48}></Image>
+              </a>
             </div>
           </div>
           <div className={style.team}>
@@ -130,8 +153,12 @@ const about = () => {
               this is alot more difficult then I thought 
             </div>
             <div className={style.socialLink}>
-              <a className={style.links} href='#'><i>GH Icon</i></a>
-              <a className={style.links} href='#'><i>LI Icon</i></a>
+              <a className={style.links} href='https://github.com/mmartin440'>
+                <Image alt="GitHub Icon" src='/GitHubIcon.png' width={48} height={48}></Image>
+              </a>
+              <a className={style.links} href='https://www.linkedin.com/in/martha-martin-pablo/'>
+                <Image alt="Linkedin Icon" src='/LinkedinIcon.png' width={48} height={48}></Image>
+              </a>
             </div>
           </div>
           <div className={style.team}>
@@ -142,8 +169,12 @@ const about = () => {
               this is alot more difficult then I thought 
             </div>
             <div className={style.socialLink}>
-              <a className={style.links} href='#'><i>GH Icon</i></a>
-              <a className={style.links} href='#'><i>LI Icon</i></a>
+              <a className={style.links} href='https://github.com/RRFernandez'>
+                <Image alt="GitHub Icon" src='/GitHubIcon.png' width={48} height={48}></Image>
+              </a>
+              <a className={style.links} href='https://www.linkedin.com/in/randyrfernandez/'>
+                <Image alt="Linkedin Icon" src='/LinkedinIcon.png' width={48} height={48}></Image>
+              </a>
             </div>
           </div>
       </div>
